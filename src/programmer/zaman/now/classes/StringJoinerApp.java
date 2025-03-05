1e277a16f4918b0238be9d106a10cea96885848c
indra <indradwiprabowo27@gmail.com>
2025-03-05 20:38:31 +0700

    lanjutan

--- src/programmer/zaman/now/classes/StringJoinerApp.java
+++ src/programmer/zaman/now/classes/StringJoinerApp.java
@@ -1,12 +1,22 @@
 package programmer.zaman.now.classes;
 
+// delimitter : pemisah
+// prefix : diawal
+// suffix : diakhir
+
 import java.util.StringJoiner;
 
 public class StringJoinerApp {
 	public static void main(String[] args) {
 
+		String[] names = {"Indra", "Dwi", "Prabowo"};
 		StringJoiner joiner = new StringJoiner(", ", "[", "]");
 
+		for (var name : names) {
+			joiner.add(name);
+		}
+
+		System.out.println(joiner.toString());
 
 	}
 }
