7605a0adebf587916b6cb3002618ee36ca11a958
indra <indradwiprabowo27@gmail.com>
2025-03-11 15:01:39 +0700

    lanjutan

---
+++ src/DateAndTime.java
@@ -0,0 +1,20 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateAndTime {
+	public static void main(String[] args) {
+
+		Date tanggal = new Date();
+
+		System.out.println(tanggal);
+
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, -10);
+		calendar.set(Calendar.MONTH, Calendar.JANUARY);
+		calendar.set(Calendar.HOUR_OF_DAY, 10);
+
+		Date result = new Date();
+		System.out.println(result);
+
+	}
+}
