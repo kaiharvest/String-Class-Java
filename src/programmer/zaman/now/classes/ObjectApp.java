0d0c49e116a9c035bb06c7a3818a4b8d4bb8b1ae
indra <indradwiprabowo27@gmail.com>
2025-03-19 10:28:38 +0700

    lanjutan

---
+++ src/programmer/zaman/now/classes/ObjectApp.java
@@ -0,0 +1,56 @@
+package programmer.zaman.now.classes;
+
+import java.util.Objects;
+
+public class ObjectApp {
+
+	public static class Data {
+		private String data;
+
+		public Data(String data) {
+			this.data = data;
+		}
+
+		public String getData() {
+			return data;
+		}
+
+		public void setData(String data) {
+			this.data = data;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (o == null || getClass() != o.getClass()) return false;
+
+			Data data1 = (Data) o;
+			return Objects.equals(data, data1.data);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hashCode(data);
+		}
+
+		@Override
+		public String toString() {
+			return "Data{" +
+					"data='" + data + '\'' +
+					'}';
+		}
+	}
+
+	public static void main(String[] args) {
+
+		execute(null);
+		execute(new Data("Indra"));
+
+	}
+
+	public static void execute(Data data) {
+
+		System.out.println(Objects.toString(data));
+		System.out.println(Objects.hashCode(data));
+
+	}
+}
